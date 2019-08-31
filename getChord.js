441d68520bfc007920a1c6e5ac6350728b6264e7
John <jrmint46@gmail.com>
2019-08-31 01:19:21 -0400

    Added more chord functions

--- getChord.js
+++ getChord.js
@@ -11,19 +11,19 @@ const getMinorChord = startNote => {
   const minorChord = [minorScale[0], minorScale[2], minorScale[4]];
   return minorChord;
 }
-  
+
 const getSusTwoChord = startNote => {
   const majorScale = getMajorScale(startNote);
   const twoChord = [majorScale[0], majorScale[1], majorScale[4]];
   return twoChord;
 }
-  
+
 const getSusFourChord = startNote => {
   const majorScale = getMajorScale(startNote);
   const fourChord = [majorScale[0], majorScale[3], majorScale[4]];
   return fourChord;
 }
-  
+
 const getDiminishedChord = startNote => {
   const minorScale = getMinorScale(startNote);
   const diminishedChord = [minorScale[0], minorScale[2]];
@@ -31,7 +31,7 @@ const getDiminishedChord = startNote => {
   diminishedChord.push(nextMinorScale[2]);
   return diminishedChord;
 }
-  
+
 const getAugmentedChord = startNote => {
   const majorScale = getMajorScale(startNote);
   const augmentedChord = [majorScale[0], majorScale[2]];
@@ -39,29 +39,50 @@ const getAugmentedChord = startNote => {
   augmentedChord.push(nextMajorScale[2]);
   return augmentedChord;
 }
-  
+
+const getSixChord = startNote => {
+  const minorScale = getMinorScale(startNote);
+  const sixChord = getMajorChord(startNote);
+  sixChord.push(minorScale[5]);
+  return sixChord;
+}
+
 const getMinorSixChord = startNote => {
-  
+  const minorScale = getMinorScale(startNote);
+  const sixChord = getMinorChord(startNote);
+  sixChord.push(minorScale[5]);
+  return sixChord;
 }
-  
+
 const getMajorSixChord = startNote => {
-    
+  const majorScale = getMajorScale(startNote);
+  const sixChord = getMajorChord(startNote);
+  sixChord.push(majorScale[5]);
+  return sixChord;
 }
-  
+
 const getSevenChord = startNote => {
   const minorScale = getMinorScale(startNote);
   const sevenChord = getMajorChord(startNote);
   sevenChord.push(minorScale[6]);
   return sevenChord;
 }
-  
-const getMajorSevenChord = startNote => {
-    
+
+const getMinorSevenChord = startNote => {
+  const minorScale = getMinorScale(startNote);
+  const sevenChord = getMinorChord(startNote);
+  sevenChord.push(minorScale[6]);
+  return sevenChord;
 }
-  
-console.log(getSusTwoChord('F'));
-console.log(getSusFourChord('F'));
+
+const getMajorSevenChord = startNote => {
+  const majorScale = getMajorScale(startNote);
+  const sevenChord = getMajorChord(startNote);
+  sevenChord.push(majorScale[6]);
+  return sevenChord;
+}
+
+
 majorNotes.forEach(x => {
-  console.log(getAugmentedChord(x));
+  console.log(getMajorSevenChord(x));
 });
-  
